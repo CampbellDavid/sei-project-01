3e406c5cba73370d05b913b5608f9bc1f81a020b
David Campbell <dcampbell092@gmail.com>
2019-12-27 12:27:45 +0000

    added key functions - commented out

--- starter-code/scripts/app.js
+++ starter-code/scripts/app.js
@@ -6,11 +6,11 @@ function init() {
 
   // Variables
 
-  const width = 16
+  const width = 16                                            // no. boxed for width
 
-  const height = 11
+  const height = 11                                           // no. boxes for height
 
-  Array(height * width).join('.').split('.').forEach(() => {
+  Array(height * width).join('.').split('.').forEach(() => {  // create Array
     const square = document.createElement('div')              // create a div element on the DOM
     square.classList.add('grid-item')                         // create a class called 'grid item' to the div element
     grid.appendChild(square)                                  // append the element as a child to the 'grid' element
@@ -19,12 +19,39 @@ function init() {
 
   // Functions
 
+  // function userPressedKey(e) {
+  //   switch (e.keyCode) {
+  //     case 39:
+  //       if (snakeLocation % width < width - 1) {
+  //         snakeLocation++
+  //       }
+  //       break
+  //     case 37:
+  //       if (snakeLocation % width > 0) {
+  //         snakeLocation--
+  //       }
+  //       break
+  //     case 40:
+  //       if (snakeLocation + width < width * width) {
+  //         snakeLocation += width 
+  //       }
+  //       break
+  //     case 38:
+  //       if (snakeLocation - width >= 0) {
+  //         snakeLocation -= width
+  //       } 
+  //       break
+  //     default:
+  //       console.log('player shouldnt move')
+  //   }
+  // }
 
 
+  // Event handlers
 
+  // window.addEventListener('keydown', userPressedKey)
 
-
-
+  
 
 
 
