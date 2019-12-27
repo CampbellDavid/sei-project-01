77e594af3fac967a17e60d4dac79035b577d0e8b
David Campbell <dcampbell092@gmail.com>
2019-12-27 13:19:00 +0000

    added movement and food

--- starter-code/scripts/app.js
+++ starter-code/scripts/app.js
@@ -3,7 +3,7 @@ function init() {
 
   const grid = document.querySelector('.grid')
 
-  const snake = []
+  const cubes = []
 
 
 
@@ -14,48 +14,72 @@ function init() {
 
   const height = 11                                           // no. boxes for height
 
+  let snakeLocation = 1
+
   Array(height * width).join('.').split('.').forEach(() => {  // create Array
-    const square = document.createElement('div')              // create a div element on the DOM
-    square.classList.add('grid-item')                         // create a class called 'grid item' to the div element
-    grid.appendChild(square)                                  // append the element as a child to the 'grid' element
+    const box = document.createElement('div')              // create a div element on the DOM
+    box.classList.add('grid-item')                         // create a class called 'grid item' to the div element
+    cubes.push(box)
+    grid.appendChild(box)                                  // append the element as a child to the 'grid' element
   })
 
 
   // Functions
 
-  // function userPressedKey(e) {
-  //   switch (e.keyCode) {
-  //     case 39:
-  //       if (snakeLocation % width < width - 1) {
-  //         snakeLocation++
-  //       }
-  //       break
-  //     case 37:
-  //       if (snakeLocation % width > 0) {
-  //         snakeLocation--
-  //       }
-  //       break
-  //     case 40:
-  //       if (snakeLocation + width < width * width) {
-  //         snakeLocation += width 
-  //       }
-  //       break
-  //     case 38:
-  //       if (snakeLocation - width >= 0) {
-  //         snakeLocation -= width
-  //       } 
-  //       break
-  //     default:
-  //       console.log('player shouldnt move')
-  //   }
+  // function newGame () {
+
   // }
+
+  cubes[snakeLocation].classList.add('userOne')
+
+  function userPressedKey(e) {
+    switch (e.keyCode) {
+      case 39:  // right arrow	
+        if (snakeLocation % width < width - 1) {
+          snakeLocation++
+        }
+        break
+      case 37:  // left arrow
+        if (snakeLocation % width > 0) {
+          snakeLocation--
+        }
+        break
+      case 40:  // down arrow
+        if (snakeLocation + width < width * height) {
+          snakeLocation += width
+        }
+        break
+      case 38:  // up arrow	
+        if (snakeLocation - width >= 0) {
+          snakeLocation -= width
+        }
+        break
+      default:
+        console.log('player shouldnt move')
+    }
+    cubes.forEach(cube => cube.classList.remove('userOne'))
+    cubes[snakeLocation].classList.add('userOne')
+    console.log('current plauer index is', snakeLocation)
+  }
+
+  function createFood() {
+    const randomNumbers = new Set()
+    while (randomNumbers.size < 1) {
+      const randomNumber = Math.floor(Math.random() * ((width * height) - 1))
+      console.log(randomNumber)
+      randomNumbers.add(randomNumber)
+      cubes[randomNumber].classList.add('food-location')
+    }
+  }
+
+  createFood()
 
 
   // Event handlers
 
-  // window.addEventListener('keydown', userPressedKey)
+  window.addEventListener('keydown', userPressedKey)
 
-  
+
 
 
 
